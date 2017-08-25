1d372b546b8593a1a1470f0299549b7ad3fba6d5
Krista Charner <kristacharner@outlook.com>
2017-08-25 08:57:00 -0400

    new html files, updated styling, images, etc

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+
+	//click function targets tags on the testimonlial page
+	$("#submitBtn").on("click", function(){
+
+        window.location.replace("https://www.purejoybody.com/thanks.html");
+	});
+
+});
